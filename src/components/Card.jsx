c4fe298fb932b3b9e246e6626c0fa992980b720e
Jhonatan Yaranga <jhoonatan_0912@hotmail.com>
2023-02-04 18:04:06 -0500

    add style classes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 function Card({ character }) {
   return (
-    <div className='border flex flex-col justify-center items-center'>
-      <h2>{character.name}</h2>
-      <img className="rounded-full" src={character.image} alt={character.name} />
-      <p>{character.origin.name}</p>
+    <div className='border flex flex-col gap-3 justify-center items-centerp p-3'>
+      <h2 className='text-3xl font-semibold'>{character.name}</h2>
+      <img className="rounded-full w-10/12 m-auto" src={character.image} alt={character.name} />
+      <p><b>Origin: </b>{character.origin.name}</p>
     </div>
   )
 }
